99d0b87fc541e102ff0f0f0178085eff3db112b7
MrBabylonian <bedirhangilgiler@gmail.com>
2025-01-16 22:17:11 +0100

    It does bite alright!

--- Snake/obj/Release/net9.0-windows/win-x64/Snake.AssemblyInfo.cs
+++ Snake/obj/Release/net9.0-windows/win-x64/Snake.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3f30605730662f8f7026618a2617d21ec2519c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
